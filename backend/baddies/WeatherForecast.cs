541a35a638f653e46ec5f7c8b2f7764105756351
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-04-05 10:19:23 -0600

    readd backend

--- backend/baddies/WeatherForecast.cs
+++ backend/baddies/WeatherForecast.cs
@@ -1,6 +1,4 @@
-using System;
-
-namespace movie
+namespace baddies
 {
     public class WeatherForecast
     {
@@ -10,6 +8,6 @@ namespace movie
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
